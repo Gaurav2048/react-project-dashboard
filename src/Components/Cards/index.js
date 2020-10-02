6aeb1c6c0a7d515c2fd6ef37905c98865e4251d8
Gunjan Kalita <gunjankalita836@gmail.com>
2020-10-02 21:54:58 +0530

    Created the layout css for dashboard

---
+++ src/Components/Cards/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './card.scss';
+
+const Card = () => {
+  return <div className="project-card">card</div>;
+};
+
+export default Card;
--- src/Components/Project/index.js
+++ src/Components/Project/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import Card from '../Cards/index';
+import './project.scss';
 
 const Project = () => {
-  return <div>Project</div>;
+  return (
+    <div className="project">
+      <h1>Projects</h1>
+      <h5>Current Projects</h5>
+      <div className="card-container">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
 };
 
 export default Project;
